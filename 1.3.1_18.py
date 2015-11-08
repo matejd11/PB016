d75dc5b8db3f9e01e790fb38ef896b3d52a99ae4
matejd11 <mdujava@gmail.com>
2015-11-08 12:26:44 +0100

    Fibonacci, iterative and recursive.

---
+++ 1.3.1_18.py
@@ -0,0 +1,21 @@
+import sys
+
+def fib_rec(i):
+	if i < 3:
+		return 1
+	return fib_rec(i - 1) + fib_rec(i - 2)
+
+def fib_iter(i):
+	array = [1, 1]
+	for j in range(2, i):
+		array.append(array[j-1] + array[j-2])
+	return array[i-1]  # pole indexujeme od nuly
+
+def main():
+	# pre veľké čísla
+	# sys.setrecursionlimit(10000)
+	print(fib_rec(27))
+	print(fib_iter(27))
+
+if __name__ == '__main__':
+    main()
