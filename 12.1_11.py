da8faba408a779aae6eea0adfc7b0cbfda6800ca
matejd11 <mdujava@gmail.com>
2015-12-21 21:10:46 +0100

    Add generatting sentences.

---
+++ 12.1_11.py
@@ -0,0 +1,53 @@
+
+def adjectives():
+    adjective = ["young"]
+    for word in adjective:
+        yield word
+
+def determiners():
+    determiner = ["the", "a"]
+    for word in determiner:
+        yield word
+
+def nouns():
+    noun = ["boy", "song"]
+    for word in noun:
+        yield word
+
+def verbs():
+    verb = ["sings"]
+    for word in verb:
+        yield word
+
+def verb_phrases():
+    for verb in verbs():
+        yield verb
+    for verb in verbs():
+        for noun_phrase in noun_phrases():
+            yield verb + " " + noun_phrase
+
+def noun_phrases2():
+    for adjective in adjectives():
+        for noun in nouns():
+            yield adjective + " " + noun
+
+def noun_phrases():
+    for determiner in determiners():
+        for noun_phrase in noun_phrases2():
+            yield determiner + " " +  noun_phrase
+    for noun_phrase in noun_phrases2():
+        yield noun_phrase
+    for noun in nouns():
+        yield noun
+
+def sentences():
+    for noun_phrase in noun_phrases():
+        for verb_phrase in verb_phrases():
+            yield noun_phrase + " " + verb_phrase
+
+def main():
+    for sentence in sentences():
+        print(sentence)
+
+if __name__ == "__main__":
+    main()
